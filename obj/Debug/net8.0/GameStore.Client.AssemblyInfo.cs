a904356c00fa9c30590822c9a24cfa22c7e88273
Neel112002 <neelshah112002@gmail.com>
2024-03-08 12:37:31 +0530

    Third  commit

--- obj/Debug/net8.0/GameStore.Client.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameStore.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7a61d2947c944a38fa404591c962e2999d227ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35a8bfefb04c00df57f0aba3f47e65ec79e17662")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
